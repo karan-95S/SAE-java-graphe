9a8efbfb89f0ecf09fa89d201e4cc8cf8b96058c
HUANG Loic <129007906+H0o0L@users.noreply.github.com>
2023-05-21 15:08:04 +0200

    Add files via upload

---
+++ graphe/implems/GrapheLAdj.java
@@ -0,0 +1,100 @@
+package graphe.implems;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import graphe.core.Arc;
+
+//import java.util.stream.Collectors;
+import java.util.HashMap;
+
+public class GrapheLAdj extends Graphe {
+	private Map<String, List<Arc>> ladj;
+
+	public GrapheLAdj() {
+		this.ladj = new HashMap<>();
+	}
+
+	public GrapheLAdj(String str) {
+		this();
+		peupler(str);
+	}
+
+	@Override
+	public void ajouterSommet(String noeud) {
+		ladj.putIfAbsent(noeud, new ArrayList<>());
+	}
+
+	@Override
+	public void ajouterArc(String source, String destination, Integer valeur) {
+		if (valeur < 0)
+			throw new IllegalArgumentException("valuation negative");
+		if (contientArc(source, destination))
+			throw new IllegalArgumentException("L'arc est deja present");
+		ajouterSommet(source);
+		ajouterSommet(destination);
+		ladj.get(source).add(new Arc(source, destination, valeur));
+	}
+
+	@Override
+	public void oterSommet(String noeud) {
+		if (contientSommet(noeud)) {
+			ladj.values().forEach(arcs -> arcs.removeIf(arc -> arc.getDestination().equals(noeud)));
+			ladj.remove(noeud);
+		}
+	}
+
+	@Override
+	public void oterArc(String source, String destination) {
+		if (!contientArc(source, destination))
+			throw new IllegalArgumentException("n'existe pas");
+		ladj.get(source).removeIf(arc -> arc.getDestination().equals(destination));
+	}
+
+	@Override
+	public List<String> getSommets() {
+		return new ArrayList<>(ladj.keySet());
+	}
+
+	@Override
+	public List<String> getSucc(String sommet) {
+		List<String> Succ = new ArrayList<>();
+		if (contientSommet(sommet))
+			for (Arc arc : ladj.get(sommet))
+				Succ.add(arc.getDestination());
+		return Succ;
+		// return ladj.getOrDefault(sommet,
+		// null).stream().map(Arc::getDestination).collect(Collectors.toList());
+	}
+
+	@Override
+	public int getValuation(String src, String dest) {
+		if (!contientSommet(src) || !contientSommet(dest))
+			throw new IllegalArgumentException("n'existe pas");
+		if (contientArc(src, dest))
+			for (Arc arc : ladj.get(src))
+				if (arc.getDestination().equals(dest))
+					return arc.getValuation();
+		return -1;
+		// return ladj.get(src).stream().filter(arc ->
+		// arc.getDestination().equals(dest)).findFirst().map(Arc::getValuation).orElse(-1);
+
+	}
+
+	@Override
+	public boolean contientSommet(String sommet) {
+		return ladj.containsKey(sommet);
+	}
+
+	@Override
+	public boolean contientArc(String source, String destination) {
+		if (contientSommet(source))
+			for (Arc arc : ladj.get(source))
+				if (arc.getDestination().equals(destination))
+					return true;
+		return false;
+		// return contientSommet(source) && ladj.get(source).stream().anyMatch(arc ->
+		// arc.getDestination().equals(destination));
+	}
+}
---
+++ graphe/implems/GrapheMAdj.java
@@ -0,0 +1,112 @@
+package graphe.implems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GrapheMAdj extends Graphe {
+	private int[][] matrice;
+	private Map<String, Integer> indices;
+	private final static int VALEUR_INIT = -1;
+
+	public GrapheMAdj() {
+		indices = new HashMap<>();
+		matrice = new int[0][0];
+	}
+
+	public GrapheMAdj(String str) {
+		this();
+		peupler(str);
+	}
+
+	public void ajouterSommet(String noeud) {
+		if (!contientSommet(noeud)) {
+			indices.put(noeud, indices.size());
+			int taille = indices.size();
+			int[][] nouvelleMatrice = new int[taille][taille];
+			for (int i = 0; i < taille; i++)
+				for (int j = 0; j < taille; j++) {
+					if (j < taille - 1 && i < taille - 1)
+						nouvelleMatrice[i][j] = matrice[i][j];
+					else
+						nouvelleMatrice[i][j] = VALEUR_INIT;
+				}
+			matrice = nouvelleMatrice;
+		}
+	}
+
+	public void ajouterArc(String source, String destination, Integer valeur) {
+		if (valeur < 0)
+			throw new IllegalArgumentException("valuation negative");
+		if (contientArc(source, destination))
+			throw new IllegalArgumentException("L'arc est deja present");
+		ajouterSommet(source);
+		ajouterSommet(destination);
+		matrice[indices.get(source)][indices.get(destination)] = valeur;
+	}
+
+	public void oterSommet(String sommet) {
+		if (contientSommet(sommet)) {
+			int indiceOterSommet = indices.get(sommet), taille = indices.size() - 1, ligne = 0, colonne = 0;
+			int[][] nouvelleMatrice = new int[taille][taille];
+			for (int i = 0; i <= taille; i++) {
+				if (i == indiceOterSommet)
+					continue;
+				for (int j = 0; j <= taille; j++) {
+					if (j == indiceOterSommet)
+						continue;
+					nouvelleMatrice[ligne][colonne++] = matrice[i][j];
+				}
+				ligne++;
+				colonne = 0;
+			}
+			matrice = nouvelleMatrice;
+			for (String S : getSommets())
+				if (indices.get(S) > indiceOterSommet)
+					indices.replace(S, indices.get(S) - 1);
+			indices.remove(sommet);
+		}
+	}
+
+	public void oterArc(String source, String destination) {
+		if (!contientArc(source, destination))
+			throw new IllegalArgumentException("n'existe pas");
+		matrice[indices.get(source)][indices.get(destination)] = VALEUR_INIT;
+	}
+
+	@Override
+	public List<String> getSommets() {
+		return new ArrayList<>(indices.keySet());
+	}
+
+	@Override
+	public List<String> getSucc(String sommet) {
+		List<String> succ = new ArrayList<>();
+		if (contientSommet(sommet)) {
+			int indice = indices.get(sommet);
+			for (String S : getSommets())
+				if (matrice[indice][indices.get(S)] != VALEUR_INIT)
+					succ.add(S);
+		}
+		return succ;
+	}
+
+	@Override
+	public int getValuation(String src, String dest) {
+		if (!contientSommet(src) || !contientSommet(dest))
+			throw new IllegalArgumentException("n'existe pas");
+		return matrice[indices.get(src)][indices.get(dest)];
+	}
+
+	@Override
+	public boolean contientSommet(String sommet) {
+		return indices.containsKey(sommet);
+	}
+
+	@Override
+	public boolean contientArc(String src, String dest) {
+		return contientSommet(src) && contientSommet(dest)
+				&& matrice[indices.get(src)][indices.get(dest)] != VALEUR_INIT;
+	}
+}
